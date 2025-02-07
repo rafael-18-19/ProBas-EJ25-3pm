a2fb3a9cf87ea8d03dfd23e82437a91e99e3d948
lap <juarezrafael748@gmail.com>
2025-02-06 20:04:26 -0600

    practica1.py

---
+++ practica en clase.py/apuntes.py
@@ -0,0 +1,28 @@
+·== igual
+·!= desigualdad
+·< menor que
+·> mayor que
+·and&
+·falso,falso = falso
+·falso, verdadero = falso
+·verdadero,falso = falso
+·verdadero,verdadero = verdadero
+·OR
+·falso,falso = falso
+·falso,verdadero = verdadero
+·verdadero,falso = verdadero
+·verdadero,verdadero = verdadero = ickkck
+·not
+·verdadero,verdadero = verdadero
+·falso,falso = verdadero
+
+conjunto = set()
+lista = list()
+tupla = tuple()
+diccionario = dict()
+
+enumerate, tupla= no puede ser otro valor en boll
+ print(bool(entero),bool(flotante),bool(string))
+ print(bool(conjunto),bool(lista),boll())
+
+ ·como gardar modificasiones en repositorio en git hub
